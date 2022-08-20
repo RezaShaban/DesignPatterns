9ca6f8698c2eadede3f8812d2b3879a30b2f1191
Reza Shaban <reza.shaban.2011@gmail.com>
2022-08-20 21:54:34 +0430

    add prototype pattern

--- DesignPatternTutorial/Program.cs
+++ DesignPatternTutorial/Program.cs
@@ -6,13 +6,17 @@ namespace DesignPatternTutorial
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("FactoryMethod");
             new FactoryMethod.ProductionLine().StartProduction();
             Console.WriteLine($"{Environment.NewLine}FactoryMethod => end production!{Environment.NewLine}");
 
+            Console.WriteLine("AbstractFactory");
             new AbstractFactory.ProductionLine().RunProductoin();
 
+            Console.WriteLine("Singleton");
             Singleton.Instance.DoSomething();
 
+            Console.WriteLine("Builder");
             new HumanCreator().CreateHuman();
         }
     }
---
+++ DesignPatternTutorial/Prototype/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace DesignPatternTutorial.Prototype
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Product ShallowCopy()
+        {
+            return (Product)this.MemberwiseClone();
+        }
+
+        public Product DeepCopy()
+        {
+            Product clone = (Product)this.MemberwiseClone();
+            clone.Name = Name;
+            clone.Age = Age;
+            return clone;
+        }
+    }
+}
